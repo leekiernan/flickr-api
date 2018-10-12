d065450989c1c776a635fbbf5f415f465e9ef30c
Lee Kiernan <dev@starlord.co>
2018-10-12 20:18:51 +0100

    Style loader

--- src/Loader/Loader.js
+++ src/Loader/Loader.js
@@ -3,6 +3,10 @@ import React from 'react'
 import './Loader.css'
 
 const Loader = (props) => (
-	<div className="Loader">Loading.</div>
+	<div className="Loader">
+    <svg className="circular" viewBox="25 25 50 50">
+      <circle className="path" cx="50" cy="50" r="20" fill="none" strokeWidth="2" strokeMiterlimit="10"/>
+    </svg>
+  </div>
 )
 export default Loader
